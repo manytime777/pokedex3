6f0b970d2e517f1c31075263f7168375ce6efe01
manytime777 <manytime@777.so-net.jp>
2016-09-08 23:39:08 +0900

    95, 96, 97, 98

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by 松岡泰史 on 2016/09/08.
+//  Copyright © 2016年 松岡泰史. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by 松岡泰史 on 2016/09/08.
+//  Copyright © 2016年 松岡泰史. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
